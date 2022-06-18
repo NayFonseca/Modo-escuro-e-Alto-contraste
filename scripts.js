99db5e5c82f37490c92a1ae1aac4d3f6c3427c30
Nayara Fonseca <88727450+NayFonseca@users.noreply.github.com>
2022-06-17 21:05:16 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function modoClaro(){
+let claro = document.body;
+claro.className = "lightMode";
+}
+
+function modoEscuro() {
+    let escuro = document.body;
+    escuro.className = "darkMode";
+  }
+  function modoAltoContraste() {
+    var contraste = document.body;
+    contraste.className = "contrastMode";
+  }
